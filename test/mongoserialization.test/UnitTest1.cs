d3d758747dbdc8b9a9d8d8965101763ab89a33d0
Tom Tutko <ttutko@outlook.com>
2019-06-03 14:40:50 -0400

    Added test project.

---
+++ test/mongoserialization.test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace mongoserialization.test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
