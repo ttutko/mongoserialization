5f9fa56f0f778df0ed5edf1dfe58c138ca18f3c7
jsodomkalm <jsodomkalm@gmail.com>
2019-06-05 13:42:40 -0400

    Bson and Newtonsoft Unit Test Framework

--- src/Models/MyModel.cs
+++ src/Models/MyModel.cs
@@ -1,4 +1,5 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,7 @@ namespace mongoserialization.Models
     {
         [BsonId]
         [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        //[JsonProperty("_id")]
         public string Id { get; set; }
 
         [BsonElement("name")]
--- test/mongoserialization.test/SerializationTest.cs
+++ test/mongoserialization.test/SerializationTest.cs
@@ -1,10 +1,13 @@
 using MongoDB.Bson;
+using MongoDB.Bson.IO;
 using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 using mongoserialization.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -12,69 +15,75 @@ namespace mongoserialization.test
 {
     public class SerializationTest
     {
-        MongoClient client;
-        IMongoDatabase db = null;
-        IMongoCollection<MyModel> collection = null;
-
+        private IMongoClient client;
+        private IMongoDatabase db = null;
+        private IMongoCollection<MyModel> collectionModel = null;
+        private IMongoCollection<BsonDocument> collectionBson = null;
         private readonly ITestOutputHelper output;
+
         public SerializationTest(ITestOutputHelper output)
         {
             this.output = output;
             client = new MongoClient("mongodb://localhost:27017");
             db = client.GetDatabase("Serialization");
-            collection = db.GetCollection<MyModel>("Models");
-
+            collectionModel = db.GetCollection<MyModel>("Models");
+            collectionBson = db.GetCollection<BsonDocument>("Models");
         }
 
         [Fact]
-        public void Bson_ImportJsonDataModel()
+        public void Bson_ImportjsonModel()
         {
-
-            var exists = collection.Find(model => model.Name.Equals("Bson Model 1")).FirstOrDefault();
+            var exists = collectionModel.Find(model => model.Name.Equals("Bson Model 1")).FirstOrDefault();
             if (exists == null)
             {
-                string jsonData;
-                using (StreamReader reader = new StreamReader(@"C:\Users\jsodomka\source\repos\mongoserialization\data\Serialization.Models.Bson.json"))
+                string json;
+                var file = @"Data\Serialization.Models.Bson.json";
+                using (StreamReader reader = new StreamReader(file))
                 {
-                    jsonData = reader.ReadToEnd();
+                    json = reader.ReadToEnd();
                 }
 
-                output.WriteLine(jsonData);
+                // Show in Output Window
+                output.WriteLine(json);
 
-                var document = BsonSerializer.Deserialize<List<MyModel>>(jsonData);
-                collection.InsertMany(document);
+                var document = BsonSerializer.Deserialize<List<MyModel>>(json);
+                collectionModel.InsertMany(document);
             }
         }
 
         [Fact]
         public void Bson_ReadDataModel()
         {
-            var result = collection.Find(model => model.Name.Equals("Bson Model 1")).FirstOrDefault();
+            var result = collectionModel.Find(model => model.Name.Equals("Bson Model 1")).FirstOrDefault();
         }
 
         [Fact]
-        public void NewtonSoft_ImportJsonDataModel()
+        public void NewtonSoft_ImportjsonModel()
         {
-            var exists = collection.Find(model => model.Name.Equals("Newton Model 1")).FirstOrDefault();
+            var exists = collectionModel.Find(model => model.Name.Equals("Newton Model 1")).FirstOrDefault();
             if (exists == null)
             {
-                string jsonData;
-                using (StreamReader reader = new StreamReader(@"C:\Users\jsodomka\source\repos\mongoserialization\data\Serialization.Models.Newton.json"))
+                string json;
+                var file = @"Data\Serialization.Models.Newton.json";
+                using (StreamReader reader = new StreamReader(file))
                 {
-                    jsonData = reader.ReadToEnd();
+                    json = reader.ReadToEnd();
                 }
 
-                output.WriteLine(jsonData);
+                // Show in Output Window
+                output.WriteLine(json);
 
-                var document = BsonSerializer.Deserialize<List<MyModel>>(jsonData);
-                collection.InsertMany(document);
+                var document = Newtonsoft.Json.JsonConvert.DeserializeObject<List<MyModel>>(json);
+                collectionModel.InsertMany(document);
             }
         }
 
         [Fact]
         public void NewtonSoft_ReadDataModel()
         {
-            var result = collection.Find(model => model.Name.Equals("Newton Model 1")).FirstOrDefault();
+            MyModel myModel = collectionModel.Find(model => model.Name.Equals("Bson Model 1")).FirstOrDefault();
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(myModel, Formatting.Indented);
+            output.WriteLine(json);            
         }
     }
 }
